fe0209ee423d32a668ed422749ace7a810694dda
Jose Santillan <mezqi14@icloud.com>
2022-01-28 01:38:13 -0500

    added some basic methods to the SimulationView and SimulationModel classes

--- src/main/java/cellsociety/models/SimulationModel.java
+++ src/main/java/cellsociety/models/SimulationModel.java
@@ -1,8 +1,33 @@
 package cellsociety.models;
 
+import cellsociety.cells.*;
+
 public class SimulationModel {
+
+  private Cell[][] myGrid;
+  public final int GRID_SIZE;
 
   public SimulationModel() {
 
   }
+  public SimulationModel(int gridSize) {
+      GRID_SIZE = gridSize;
+  }
+
+  private void init() {
+
+  }
+
+  private void step() {
+
+  }
+
+  public Cell[][] getGrid() {
+    return myGrid;
+  }
+
+  private void updateGrid() {
+
+  }
+
 }
--- src/main/java/cellsociety/views/SimulationView.java
+++ src/main/java/cellsociety/views/SimulationView.java
@@ -1,8 +1,24 @@
 package cellsociety.views;
 
+import javafx.scene.Node;
+
 public class SimulationView {
 
   public SimulationView() {
 
   }
+
+  private void initUI() {
+
+  }
+  private void update() {
+
+  }
+
+  private Node initButtons() {
+    return null;
+  }
+
+  
+
 }
