d8642e00c78205cb6a2e21d1f06a15cf2b504473
Cynthia France <cf219@duke.edu>
2022-01-29 14:46:51 -0500

    specified which cells were for what (just for game of life and percolation simulations

--- src/main/java/cellsociety/cells/FilledCell.java
+++ src/main/java/cellsociety/cells/FilledCell.java
@@ -1,9 +1,8 @@
 package cellsociety.cells;
 
-import javafx.scene.paint.Color;
-
 public class FilledCell extends Cell {
 
+  //FOR PERCOLATION
   public FilledCell(int x, int y, int initState) {
     super(x, y, initState);
   }
--- src/main/java/cellsociety/cells/PercolatingCell.java
+++ src/main/java/cellsociety/cells/PercolatingCell.java
@@ -1,8 +1,9 @@
 package cellsociety.cells;
 
-public class OpenedCell extends Cell {
+public class PercolatingCell extends Cell {
 
-  public OpenedCell(int x, int y, int initState) {
+  //FOR PERCOLATION
+  public PercolatingCell(int x, int y, int initState) {
     super(x, y, initState);
   }
 }
