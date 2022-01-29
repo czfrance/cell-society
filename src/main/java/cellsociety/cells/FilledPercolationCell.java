1b706ce0233df3625bd36fab3ab2f5c368093ad6
Cynthia France <cf219@duke.edu>
2022-01-29 14:50:25 -0500

    renamed cell to make it more clear

---
+++ src/main/java/cellsociety/cells/FilledPercolationCell.java
@@ -0,0 +1,9 @@
+package cellsociety.cells;
+
+public class FilledPercolationCell extends Cell {
+
+  //FOR PERCOLATION
+  public FilledPercolationCell(int x, int y, int initState) {
+    super(x, y, initState);
+  }
+}
